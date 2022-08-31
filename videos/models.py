7a47fe08432a506972a3fca5e004d60c2cccac7c
goodboyz <goodnewsukaegbu13@gmail.com>
2022-08-31 13:30:41 +0100

    video model updated

--- videos/models.py
+++ videos/models.py
@@ -1,7 +1,25 @@
+from time import time
 from weakref import proxy
 from django.db import models
 from django.utils import timezone
+from django.utils.text import slugify
 # Create your models here.
+
+
+class VideoQuerySet(models.QuerySet):
+    def published(self):
+        now = timezone.now()
+        return self.filter(
+            state = Video.VideoStateOptions.PUBLISH,
+            publish_timestamp__lte = now
+        )
+
+class VideoManager(models.Manager):
+    def get_queryset(self):
+        return VideoQuerySet(self.model, using= self._db)
+
+    def published(self):
+        return self.get_queryset().published()    
 
 class Video(models.Model):
     class VideoStateOptions(models.TextChoices):
@@ -12,11 +30,16 @@ class Video(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     slug = models.SlugField(blank=True, null=True)
-    video_id = models.CharField(max_length=255)
+    video_id = models.CharField(max_length=255, unique=True)
     active = models.BooleanField(default=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
     state = models.CharField(max_length=2, choices=VideoStateOptions.choices,
     default = VideoStateOptions.DRAFT)
     publish_timestamp = models.DateTimeField(auto_now_add=False, auto_now = False, blank = True, null = True)
+
+
+    objects = VideoManager()
 
     @property
     def is_published(self):
@@ -24,10 +47,11 @@ class Video(models.Model):
 
     def save(self, *args, **kwargs):
         if self.state == self.VideoStateOptions.PUBLISH and self.publish_timestamp is None:
-            print("save as published")
             self.publish_timestamp = timezone.now()
         elif self.state == self.VideoStateOptions.DRAFT:
             self.publish_timestamp = None
+        if self.slug is None:
+            self.slug = slugify(self.title)    
         super().save(*args, **kwargs)        
 
 class VideoAllProxy(Video):
--- videos/tests.py
+++ videos/tests.py
@@ -1,12 +1,21 @@
 from django.test import TestCase
-
+from django.utils import timezone
 from .models import Video
+
+from django.utils.text import slugify
 # Create your tests here.
 
 
 class VideoModeTestCase(TestCase):
     def setUp(self):
-        Video.objects.create(title = 'This is a title')
+        self.obj_a = Video.objects.create(title = 'This is a title', video_id = 'regs')
+        self.obj_b = Video.objects.create(title = 'This is title' , state=Video.VideoStateOptions.PUBLISH,
+        video_id = 'kjsjss')
+
+    def test_slug_field(self):
+        title =  self.obj_a.title
+        test_slug = slugify(title)
+        self.assertEqual(test_slug, self.obj_a.slug)
 
     def test_valid_title(self):
         title = 'This is a title'
@@ -16,4 +25,25 @@ class VideoModeTestCase(TestCase):
 
     def test_created_account(self):
         qs = Video.objects.all()
+        self.assertEqual(2, qs.count())
+
+    def  test_draft_case(self):
+        qs = Video.objects.filter(state = Video.VideoStateOptions.DRAFT)
         self.assertEqual(1, qs.count())
+
+    def  test_publish_case(self):
+        qs = Video.objects.filter(state = Video.VideoStateOptions.PUBLISH)
+        now = timezone.now()
+        published_qs = Video.objects.filter(
+            state = Video.VideoStateOptions.PUBLISH,
+            publish_timestamp__lte = now
+        )
+        
+        self.assertTrue(published_qs.exists)  
+
+
+    def test_publish_manager(self):
+        published_qs = Video.objects.all().published()
+        published_qs_2 = Video.objects.published()
+        self.assertTrue(published_qs.exists)               
+        self.assertEqual(published_qs.count(), published_qs_2.count())               
