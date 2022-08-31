1372f5ec08d3011d17af5882fde5f1d4b4c28919
goodboyz <goodnewsukaegbu13@gmail.com>
2022-08-31 15:02:05 +0100

    models and test rewritten to suit the current state

--- videos/tests.py
+++ videos/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.utils import timezone
 from .models import Video
+from djangoflix.db.models import PublishStateOptions
 
 from django.utils.text import slugify
 # Create your tests here.
@@ -8,12 +9,13 @@ from django.utils.text import slugify
 
 class VideoModeTestCase(TestCase):
     def setUp(self):
-        self.obj_a = Video.objects.create(title = 'This is a title', video_id = 'regs')
-        self.obj_b = Video.objects.create(title = 'This is title' , state=Video.VideoStateOptions.PUBLISH,
-        video_id = 'kjsjss')
+        self.obj_a = Video.objects.create(
+            title='This is a title', video_id='regs')
+        self.obj_b = Video.objects.create(title='This is title', state=PublishStateOptions.PUBLISH,
+                                          video_id='kjsjss')
 
     def test_slug_field(self):
-        title =  self.obj_a.title
+        title = self.obj_a.title
         test_slug = slugify(title)
         self.assertEqual(test_slug, self.obj_a.slug)
 
@@ -22,28 +24,26 @@ class VideoModeTestCase(TestCase):
         qs = Video.objects.filter(title=title)
         self.assertTrue(qs.exists())
 
-
     def test_created_account(self):
         qs = Video.objects.all()
         self.assertEqual(2, qs.count())
 
-    def  test_draft_case(self):
-        qs = Video.objects.filter(state = Video.VideoStateOptions.DRAFT)
+    def test_draft_case(self):
+        qs = Video.objects.filter(state=PublishStateOptions.DRAFT)
         self.assertEqual(1, qs.count())
 
-    def  test_publish_case(self):
-        qs = Video.objects.filter(state = Video.VideoStateOptions.PUBLISH)
+    def test_publish_case(self):
+        qs = Video.objects.filter(state=PublishStateOptions.PUBLISH)
         now = timezone.now()
         published_qs = Video.objects.filter(
-            state = Video.VideoStateOptions.PUBLISH,
-            publish_timestamp__lte = now
+            state=PublishStateOptions.PUBLISH,
+            publish_timestamp__lte=now
         )
-        
-        self.assertTrue(published_qs.exists)  
 
+        self.assertTrue(published_qs.exists)
 
     def test_publish_manager(self):
         published_qs = Video.objects.all().published()
         published_qs_2 = Video.objects.published()
-        self.assertTrue(published_qs.exists)               
-        self.assertEqual(published_qs.count(), published_qs_2.count())               
+        self.assertTrue(published_qs.exists)
+        self.assertEqual(published_qs.count(), published_qs_2.count())
